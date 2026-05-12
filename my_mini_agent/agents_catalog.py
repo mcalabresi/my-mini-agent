4301712da6b8c73fc18f832fa205625dfe589ef8
Marcello Calabresi <marcello.calabresi@gmail.com>
2026-05-12 09:17:29 +0200

    adding descriptions for agents

--- my_mini_agent/agents_catalog.py
+++ my_mini_agent/agents_catalog.py
@@ -3,13 +3,22 @@ from typing import Any
 available_agents: list[dict[str, Any]] = [
     {
         "name": "Bong",
+        "description": """
+        Bong is no-nonsense assistant. He provides brief answers and is quick.
+        He may not be the best choice to discuss directly to humans as his manners may be considered rude but he may be the best pick
+        when you need a quick and concise answer.
+        """,
         "system_prompt": "You are a reticent assistant, you answer clearly and briefly with simple words",
         "context": ["current_date_time", "user_context"],
         "skills": [],
-        "tools": [],
+        "tools": ["read_note"],
     },
     {
         "name": "Clive",
+        "description": """"
+        Clive is a creative writer for children stories. He is humorous and has lots of competence in inventing up new plots.
+        Whenever there is the need to craft stories for children, he is the go-to agent.
+        """,
         "system_prompt": "You are a writer of funny stories for children, you put humour and creativity",
         "context": [],
         "skills": ["long-term-memory", "obsidian-markdown-lite"],
@@ -23,6 +32,10 @@ available_agents: list[dict[str, Any]] = [
     },
     {
         "name": "Eddie",
+        "description": """
+        Eddie is a meticolous editor of children books. He is able to point out all the inconsistencies and flaws of a children story.
+        It's the agent you need when you need to review a children story.
+        """,
         "system_prompt": "You are an editor of children books. You are challenging the authors.",
         "context": [],
         "skills": ["children-books-critic"],
